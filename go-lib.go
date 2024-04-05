670444d035c3993d13706917553202d258334d49
manhnvan <manh12344444444@gmail.com>
2024-04-05 18:10:28 +0700

    init commit

---
+++ go-lib.go
@@ -0,0 +1,17 @@
+/**
+ * Author: Mitch Allen
+ * File: go-lib.go
+ */
+
+package lib
+
+// Returns the sum of two numbers
+func Add(a int, b int) int {
+	return a + b
+}
+
+// Returns the difference between two numbers
+func Subtract(a int, b int) int {
+	return a - b
+}
+
